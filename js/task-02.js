80c687fc25f971391932a7bde5196acd1dbf4d03
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-03 23:11:47 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const itemsRefs = ingredients.map(item => {
+  const listItemRef = document.createElement('li');
+  listItemRef.classList.add('item');
+  listItemRef.textContent = `${item}`;
+  return listItemRef;
+});
+ingredientsListRef.append(...itemsRefs);
