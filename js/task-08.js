6cd812b0a8ee89c184131862b6b0ce3c98ebfed9
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-05 20:28:09 +0300

    Update task-09.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const registrationForm = document.querySelector('.login-form');
+const onFormSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  const formData = { email: email.value, password: password.value };
 
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  } else {
+    console.log(`email: ${formData.email}, Password: ${formData.password}`);
+    event.currentTarget.reset();
+  }
+};
+registrationForm.addEventListener('submit', onFormSubmit);
