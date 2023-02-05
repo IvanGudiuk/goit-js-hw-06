ce6bb853c03873847ea7525fc20954a7363cb705
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-05 21:18:47 +0300

    Update home task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const color = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  bodyBackgroundRef.style.backgroundColor = color;
+  colorValue.textContent = color;
 }
+const bodyBackgroundRef = document.body;
+bodyBackgroundRef.setAttribute('style', 'background-color:#FFFAFA');
+const colorValue = document.querySelector('.color');
+const colorChangeBtn = document.querySelector('.change-color');
+colorChangeBtn.addEventListener('click', getRandomHexColor);
