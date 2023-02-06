1e62fd8dad4eb833d0c516acb742f013a3d62a8a
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-06 16:15:32 +0300

    corrected mistakes

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 const boxesContainer = document.querySelector('#boxes');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
@@ -6,12 +11,11 @@ const createBoxes = () => {
   const createdBoxes = [];
   const sum = amountInput.value * 10 + 30;
   for (let k = 30; k < sum; k += 10) {
-    let data = `<div style="background-color: #${Math.floor(
-      Math.random() * 16777215
-    ).toString(16)}; width: ${k}px; height: ${k}px"></div>`;
+    let data = `<div style="background-color: ${getRandomHexColor()}; width: ${k}px; height: ${k}px"></div>`;
     createdBoxes.push(data);
   }
   const string = createdBoxes.join('');
+  boxesContainer.innerHTML = '';
   boxesContainer.insertAdjacentHTML('afterbegin', string);
 };
 const destroyBoxes = () => {
