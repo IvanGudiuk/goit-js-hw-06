96d447cf186c97776d38b1fe82bc5842123e1f01
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-06 16:29:41 +0300

    corrected mistakes

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,17 @@
 function getRandomHexColor() {
-  const color = `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-  bodyBackgroundRef.style.backgroundColor = color;
-  colorValue.textContent = color;
 }
+
 const bodyBackgroundRef = document.body;
 bodyBackgroundRef.setAttribute('style', 'background-color:#FFFAFA');
 const colorValue = document.querySelector('.color');
 const colorChangeBtn = document.querySelector('.change-color');
-colorChangeBtn.addEventListener('click', getRandomHexColor);
+
+const changeColor = () => {
+  const color = getRandomHexColor();
+  bodyBackgroundRef.style.backgroundColor = color;
+  colorValue.textContent = color;
+};
+colorChangeBtn.addEventListener('click', changeColor);
