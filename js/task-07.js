b89794a53ce64665245febe75f7a9e8a7aaeba8d
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-06 16:32:50 +0300

    corrected mistakes

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const textSizeSwitch = document.querySelector('#font-size-control');
-const wordOnChange = document.querySelector('#text');
-wordOnChange.setAttribute('style', 'font-size: 16px');
+const word = document.querySelector('#text');
+word.setAttribute('style', 'font-size: 16px');
 const handleSizeSwitch = () => {
-  wordOnChange.style.fontSize = textSizeSwitch.value + 'px';
+  word.style.fontSize = textSizeSwitch.value + 'px';
 };
 textSizeSwitch.addEventListener('input', handleSizeSwitch);
